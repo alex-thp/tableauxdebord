2a4bb266fe8a07fe223f9951d2ab59a479cc9455
alex-thp <43551608+alex-thp@users.noreply.github.com>
2025-05-14 17:13:38 +0200

    prod environnement

--- srcs/backend/src/main.ts
+++ srcs/backend/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
 cron.schedule('59 23 * * *', async function() {
   await updateBaseService.retrieveBase();
 });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
